e05292a24a005263e8d3acb82573a72845b935a0
Leonardo Lunario <lvlunario@lvlunario-asustufdash.CG-24D>
2025-05-29 18:58:53 +0800

    feat: add concatenated strings for greeting

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,9 +8,14 @@
 */
 
 // Add your code right below, good luck!
+firstName = "Carlos"
+lastName = "Stevenson"
+thisYear = 1965
+birthYear = 1947
+greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old."
 
-
-
+fullName = firstName + " " + lastName
+age = thisYear - birthYear
 
 
 // Do not change this
