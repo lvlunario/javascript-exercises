28155c29583538824ebdfac2697cf1ccacd826bc
Leonardo Lunario <leonardolunario@gmail.com>
2025-06-15 09:29:55 +0800

    feat: add methods to reverse string, test and packages

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    console.log("Original:", str);
+
+    /*
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    */
+
+    // Simplified above
+    let reversed = str.split("").reverse().join("");
+
+    console.log("Reversed:", reversed)
+    
+    return reversed;
+    //return joinArray;
 };
 
 // Do not edit below this line
