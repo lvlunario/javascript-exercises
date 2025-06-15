3daee061805ab4ab85137ddcb9bdf0591fa8eadd
Leonardo Lunario <leonardolunario@gmail.com>
2025-06-15 08:21:53 +0800

    feat: add while loop to repeatString function, test scripts, and packages

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) return 'ERROR';
 
+    let result = '';
+    while (times > 0) {
+        result += string;
+        times--;
+    }
+    console.log(`Result: "${result}"`);
+    return result;
 };
 
 // Do not edit below this line
