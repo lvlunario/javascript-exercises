a4267c6cd29a55ee7cf101b739b36bcbe63ffb9e
Leonardo Lunario <leonardolunario@gmail.com>
2025-06-17 21:59:33 +0800

    feat: add leap year logic, modify test, and add test packages

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+/*
+Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+
+function leapYears with argument year
+if year is divisible by 4 and 
+not divisible by 100 
+(unless it's divisible by 400)
+return true
+
+*/
+
+/*
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+*/
+
+return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 
 };
 
