65bd99a484a3b16f884430671d28920815458ce5
Leonardo Lunario <leonardolunario@gmail.com>
2025-07-03 20:45:35 +0800

    feat: add function to remove alphanumeric characters, convert string to lowercase, and reverse string to compare if it is a palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr = str
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '');
 
+    // Compare the cleaned string to its reverse
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
