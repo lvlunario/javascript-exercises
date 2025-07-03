66924f0c6818a05b52be95b294cad6c6b4942807
Leonardo Lunario <leonardolunario@gmail.com>
2025-07-03 20:21:03 +0800

    feat: add multiplication, power, and factorial to calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,16 +10,23 @@ const sum = function(numbers) {
 	return numbers.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
