b84be15646a9d430bc1f48a67563e073b2726dfa
Leonardo Lunario <leonardolunario@gmail.com>
2025-07-04 09:41:06 +0800

    npm init and implement findTheOldest from array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
 const findTheOldest = function() {
-
+    return people.reduce((oldest, person) => {
+        const age = person.yearOfDeath - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
