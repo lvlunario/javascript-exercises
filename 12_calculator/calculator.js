053c76ac9cc253f3d6e41416187922f7b7d421d3
Leonardo Lunario <leonardolunario@gmail.com>
2025-07-04 09:06:01 +0800

    chore: revise factorial function to start the product in a descending order

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -19,11 +19,16 @@ const power = function(base, exponent) {
 };
 
 const factorial = function(n) {
-	if (n < 0) return undefined;
-  if (n === 0 || n === 1) return 1;
+	// if (n < 0) return undefined;
+  // if (n === 0 || n === 1) return 1;
 
+  // let result = 1;
+  // for (let i = 2; i <= n; i++) {
+  //   result *= i;
+  // }
+  // return result;
   let result = 1;
-  for (let i = 2; i <= n; i++) {
+  for (let i = n; i > 0; i--) {
     result *= i;
   }
   return result;
